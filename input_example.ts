3d75b7c6ea63aee83651378591ee06c708004bbf
Poxios <poxios0310@gmail.com>
2024-02-11 15:09:29 +0900

    done

--- input_example.ts
+++ input_example.ts
@@ -19,7 +19,7 @@ type InputType = {
 
 
 const input: InputType = {
-  minSubjectsCount: 2,
+  minSubjectsCount: 3,
   groups: [
     {
       name: "프로그래밍언어", isMandatory: true, subjects: [
@@ -44,11 +44,61 @@ const input: InputType = {
       ]
     },
     {
-      name: "마케팅", isMandatory: true, subjects: [
+      name: "마케팅", isMandatory: false, subjects: [
         {
           name: "마케팅", time: [
             { day: "TUE", start: "13:30", end: "14:45" },
             { day: "THU", start: "13:30", end: "14:45" }
+          ]
+        }
+      ]
+    },
+    {
+      name: "Entrepreneurship", isMandatory: false, subjects: [
+        {
+          name: "Entrepreneurship (최금선)", time: [
+            { day: "THU", start: "10:30", end: "11:45" },
+            { day: "THU", start: "12:00", end: "13:15" }
+          ]
+        },
+        {
+          name: "Entrepreneurship (박병호)", time: [
+            { day: "TUE", start: "15:00", end: "16:15" },
+            { day: "TUE", start: "16:30", end: "17:45" }
+          ]
+        },
+        {
+          name: "Entrepreneurship (김영수)", time: [
+            { day: "MON", start: "15:00", end: "16:15" },
+            { day: "WED", start: "15:00", end: "16:15" },
+          ]
+        },
+        {
+          name: "Entrepreneurship (김영수)", time: [
+            { day: "MON", start: "16:30", end: "17:45" },
+            { day: "WED", start: "16:30", end: "17:45" },
+          ]
+        }
+      ]
+    },
+    {
+      name: "생산시스템관리", isMandatory: false, subjects: [
+        {
+          name: "생산시스템관리(가)", time: [
+            { day: "MON", start: "10:30", end: "11:45" },
+            { day: "THU", start: "12:00", end: "13:15" }
+          ]
+        },
+        {
+          name: "생산시스템관리(나)", time: [
+            { day: "MON", start: "12:00", end: "13:15" },
+            { day: "THU", start: "13:30", end: "14:45" }
+          ]
+        },
+        {
+          name: "생산시스템관리(다)", time: [
+            { day: "TUE", start: "12:00", end: "13:15" },
+            { day: "THU", start: "16:30", end: "17:45" },
           ]
         }
       ]
